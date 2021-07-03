0d930ae312da085bad0cc6a2d0e37b107cf6d37a
GursahajKohli <66899035+GursahajKohli@users.noreply.github.com>
2021-07-03 13:54:03 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-d = webdriver.Chrome('/usr/local/bin/chromedriver')
-
-d.get("https://www.techwithtim.net/")
-print(d.title)
-d.close()
+options = Options()
+options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('disable-infobars')
+options.add_argument("--disable-extensions")
+driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
+driver.get("http://github.com/")
+print ('Page title is ' + driver.title)
