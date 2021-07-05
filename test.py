ccbe63fe7d90a614ba151c23a1e233fea6c4320b
GursahajKohli <66899035+GursahajKohli@users.noreply.github.com>
2021-07-05 10:53:15 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+#Intializing the webdriver for selenium
 options = Options()
 options.add_argument("--headless")
 options.add_argument('--no-sandbox')
@@ -8,8 +11,15 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_argument('disable-infobars')
 options.add_argument("--disable-extensions")
 driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
-import pandas as pd
-config = {'url' : 'https://vectorinstitute.bamboohr.com/jobs/', 'links' : '//div[contains(@itemtype, "http://schema.org/JobPosting")]//div//a'}
+
+#Config & job data, will be fetched in future from config files
+config = {'url' : 'https://vectorinstitute.bamboohr.com/jobs/', 'links' : '//div[contains(@itemtype, "http://schema.org/JobPosting")]//div//a', 'company' : 'Vector Institute'}
+job_details = {'title' : '//div[contains(@class, "col-xs-12 col-sm-8 col-md-12")]//h2/text()', 'location' : '//span[contains(@class, "ResAts__card-subtitle")]/text()', 'description' : '//div[contains(@class, "col-xs-12 BambooRichText")]', 'logo', : 'https://images3.bamboohr.com/93316/logos/cropped.jpg?v=29', }
+column_names = ['company', 'description', 'location', 'logo', 'reference', 'remote', 'title', 'url']
+
+job_csv = pd.DataFrame(columns = column_names)
+#final_csv = pd.DataFrame()
+#Start fetching 
 driver.get(config['url'])
 print(driver.title)
 print("Job links found as")
@@ -18,4 +28,47 @@ print(search)
 for i in search:
   print(i)
   print(i.get_attribute("href"))
-print("Done!")
+  link = i.get_attribute("href")
+  parse_job(link, config, job_details, job_csv, column_names)
+  
+job_csv.to_csv('Jobs.csv')
+  
+def parse_job(link, config, job_details,  job_csv, column_names):
+  print("Parsing job at url  :: ", link) 
+  
+  driver.get(link)
+  
+  job_title = driver.find_element_by_xpath(job_details['title'])
+  
+  job_location = driver.get_element_by_xpath(job_details['location'])
+  
+  job_desription = driver.get_element_by_xpath(job_details['description'])
+  
+  job_logo = job_details['logo']
+  
+  job_company = config['company']
+  
+  if 'reference' in job_details.keys():
+    job_reference = driver.get_element_by_xpath(job_details['reference'])
+  else:
+    job_reference = link
+    
+  remote_fetcher = job_location.split()
+  
+  for i in remote_fetcher :
+    if i.lower() == 'remote':
+      job_remote = 'True'
+      break
+    else:
+      job_remote = 'False'
+  
+  job_url = link
+  
+  job_list = [job_company, job_description, job_location, job_logo, job_reference, job_remote, job_title, job_url]
+  df2 = pd.DataFrame(job_list, columns = column_names)
+  job_csv.append(df2)
+  print("Job fetched from ::", link)
+  
+
+  
+ 
