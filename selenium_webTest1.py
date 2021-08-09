dd8561af526034bf6a0f3385fb12d7433f10c7ec
GursahajKohli <66899035+GursahajKohli@users.noreply.github.com>
2021-08-09 19:27:38 -0400

    Update selenium_webTest1.py

--- selenium_webTest1.py
+++ selenium_webTest1.py
@@ -59,7 +59,7 @@ class SeleniumWeb:
         job_urls = []
         action = ActionChains(self.driver)
 
-        counter = 5
+        counter = 2
 
         for job in job_links:
             if counter < 0:
