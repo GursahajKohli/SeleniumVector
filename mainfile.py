639edd3362ccb5b25134db98480056a5494bbe65
GursahajKohli <66899035+GursahajKohli@users.noreply.github.com>
2021-08-16 21:43:18 -0400

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -110,7 +110,7 @@ def createXML():
 
 filelist = os.listdir("config/src/src")
 print("companies to be scraped :: ", filelist)
-filelist = ['canvass.scraper.config']
+filelist = ['telus.scraper.config']
 for file in filelist:
     config = configparser.ConfigParser()
     file = "config/src/src/" + file
