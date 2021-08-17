f8e4f3ba90173e765a83ba37ffc1f6f518ee9c92
GursahajKohli <66899035+GursahajKohli@users.noreply.github.com>
2021-08-16 21:50:17 -0400

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -110,7 +110,7 @@ def createXML():
 
 filelist = os.listdir("config/src/src")
 print("companies to be scraped :: ", filelist)
-filelist = ['telus.scraper.config']
+filelist = ['sunlife.scraper.config']
 for file in filelist:
     config = configparser.ConfigParser()
     file = "config/src/src/" + file
